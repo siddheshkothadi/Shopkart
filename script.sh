a76040da7395d43f14699e7a309b7ac696579844
Siddhesh Kothadi <55179845+siddheshkothadi@users.noreply.github.com>
2020-09-07 18:45:39 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,17 @@
+# Setting up git configurations
+git config --global user.name "siddheshkothadi"
+git config --global user.email "realsiddheshko@gmail.com"
 
+# Make a directory
+mkdir ~/apk
+
+# Copy apk
+cp app/build/outputs/apk/debug/app-debug.apk ~/apk
+
+# Change git branch
+git fetch origin
+git checkout apk
+
+# Move apk
+# mv ~/apk/app-debug.apk ~
+pwd
